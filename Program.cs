a5ebc5c3e69baaa53914d1718903dab1b5fcc8ba
Berdnyk-O <berdnyk.oleksandr.v@gmail.com>
2025-03-13 20:48:06 +0200

    add saving of telegram token in json file

--- Program.cs
+++ Program.cs
@@ -1,2 +1,11 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using Microsoft.Extensions.Configuration;
+
+var builder = new ConfigurationBuilder();
+builder.SetBasePath(Directory.GetCurrentDirectory())
+       .AddJsonFile("appsettings.json", optional: false, reloadOnChange: true);
+
+IConfiguration config = builder.Build();
+
+var batchSize = config["TelegramBotToken"];
+
+Console.WriteLine($"Token:  {batchSize}");
