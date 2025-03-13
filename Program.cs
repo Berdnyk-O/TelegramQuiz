50648816e0c068d620654ab4777fc49934a9d7d9
Berdnyk-O <berdnyk.oleksandr.v@gmail.com>
2025-03-13 21:36:47 +0200

    add message and command processing

--- Program.cs
+++ Program.cs
@@ -1,5 +1,8 @@
 ﻿using Microsoft.Extensions.Configuration;
 using Telegram.Bot;
+using Telegram.Bot.Types;
+using Telegram.Bot.Types.Enums;
+using Telegram.Bot.Types.ReplyMarkups;
 
 var builder = new ConfigurationBuilder();
 builder.SetBasePath(Directory.GetCurrentDirectory())
@@ -11,7 +14,99 @@ var token = config["TelegramBotToken"];
 
 Console.WriteLine($"Token:  {token}");
 
+using var cts = new CancellationTokenSource();
+var bot = new TelegramBotClient(token, cancellationToken: cts.Token);
 
-var bot = new TelegramBotClient(token);
 var me = await bot.GetMe();
-Console.WriteLine($"Hello, World! I am user {me.Id} and my name is {me.FirstName}.");
+
+bot.OnMessage += OnMessage;
+bot.OnUpdate += OnUpdate;
+
+while (Console.ReadKey(true).Key != ConsoleKey.Escape) ;
+cts.Cancel();
+
+async Task OnMessage(Message msg, UpdateType type)
+{
+    if (msg.Text is not { } text)
+        Console.WriteLine($"Received a message of type {msg.Type}");
+    else if (text.StartsWith('/'))
+    {
+        var space = text.IndexOf(' ');
+        if (space < 0) space = text.Length;
+        var command = text[..space].ToLower();
+        if (command.LastIndexOf('@') is > 0 and int at)
+            if (command[(at + 1)..].Equals(me.Username, StringComparison.OrdinalIgnoreCase))
+                command = command[..at];
+            else
+                return;
+        await OnCommand(command, text[space..].TrimStart(), msg);
+    }
+    else
+        await OnTextMessage(msg);
+}
+
+async Task OnTextMessage(Message msg)
+{
+    Console.WriteLine($"Received text '{msg.Text}' in {msg.Chat}");
+    await OnCommand("/start", "", msg);
+}
+
+async Task OnCommand(string command, string args, Message msg)
+{
+    Console.WriteLine($"Received command: {command} {args}");
+    switch (command)
+    {
+        case "/start":
+            await bot.SendMessage(msg.Chat, """
+                <b><u>Bot menu</u></b>:
+                /inline_buttons - send inline buttons
+                /keyboard       - send keyboard buttons
+                /remove         - remove keyboard buttons
+                /poll           - send a poll
+                /reaction       - send a reaction
+                """, parseMode: ParseMode.Html, linkPreviewOptions: true,
+                replyMarkup: new ReplyKeyboardRemove());
+            break;
+        case "/inline_buttons":
+            await bot.SendMessage(msg.Chat, "Inline buttons:", replyMarkup: new InlineKeyboardButton[][] {
+                ["1.1", "1.2", "1.3"],
+                [("WithCallbackData", "CallbackData"), ("WithUrl", "https://github.com/TelegramBots/Telegram.Bot")]
+            });
+            break;
+        case "/keyboard":
+            await bot.SendMessage(msg.Chat, "Keyboard buttons:", replyMarkup: new string[][] { ["1.1", "1.2", "1.3"], ["2.1", "2.2"] });
+            break;
+        case "/remove":
+            await bot.SendMessage(msg.Chat, "Removing keyboard", replyMarkup: new ReplyKeyboardRemove());
+            break;
+        case "/poll":
+            await bot.SendPoll(msg.Chat, "Question", ["Option 0", "Option 1", "Option 2"], isAnonymous: false, allowsMultipleAnswers: true);
+            break;
+        case "/reaction":
+            await bot.SetMessageReaction(msg.Chat, msg.Id, ["❤"], false);
+            break;
+    }
+}
+
+async Task OnUpdate(Update update)
+{
+    switch (update)
+    {
+        case { CallbackQuery: { } callbackQuery }: await OnCallbackQuery(callbackQuery); break;
+        case { PollAnswer: { } pollAnswer }: await OnPollAnswer(pollAnswer); break;
+        default: Console.WriteLine($"Received unhandled update {update.Type}"); break;
+    }
+    ;
+}
+
+async Task OnCallbackQuery(CallbackQuery callbackQuery)
+{
+    await bot.AnswerCallbackQuery(callbackQuery.Id, $"You selected {callbackQuery.Data}");
+    await bot.SendMessage(callbackQuery.Message!.Chat, $"Received callback from inline button {callbackQuery.Data}");
+}
+
+async Task OnPollAnswer(PollAnswer pollAnswer)
+{
+    if (pollAnswer.User != null)
+        await bot.SendMessage(pollAnswer.User.Id, $"You voted for option(s) id [{string.Join(',', pollAnswer.OptionIds)}]");
+}
